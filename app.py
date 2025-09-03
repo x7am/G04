d492162f8c707e48e9805c4d01c43233a548c92f
Saeed <saeedyahyaah@gmail.com>
2025-09-03 14:57:18 +0800

    fix

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///C:/Users/saeed/OneDrive/Desktop/Rented/rented.db"
-db = SQLAlchemy(app) 
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,32 +15,44 @@ class User(db.Model):
 
     def __repr__(self):
         return f"<User {self.username}>"
-    
+
 @app.route("/")
 def home():
-    title = "Rented"
-    return render_template("index.html", title=title)
-
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
-        return f"Hello, {username}! You tried to log in."
-    return render_template("login.html", title="Login")
+    return render_template("index.html", title="Rented")
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
+
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            return "Username already exists!"
+
         new_user = User(username=username, password=password)
         db.session.add(new_user)
         db.session.commit()
-        return f"User {username} created!"
+        return redirect(url_for("login"))
+
     return render_template("signup.html", title="Sign Up")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            return redirect(url_for("home"))
+        else:
+            error = "Incorrect username or password!"
+
+    return render_template("login.html", title="Login", error=error)
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
+        db.create_all()  
     app.run(debug=True)
